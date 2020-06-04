6c77f8f069b5b1f40c0a19562b3b77030b9b02ba
Manu <manuel.hubert@hs-furtwangen.de>
2020-06-04 15:12:48 +0200

    update

--- Aufgabe05/interface.ts
+++ Aufgabe05/interface.ts
@@ -3,16 +3,16 @@ interface ArtikelMen {
     img: string;
     name: string;
     description: string;
-    preis: string;
+    preis: number;
 }
 
 //Artikel Men
-let artikel1: ArtikelMen = { img: "shirt1.jpg", name: "Shirt Nike", description: "Sytlish Nike shirt", preis: "29,99€" };
-let artikel2: ArtikelMen = { img: "shirt2.jpg", name: "Long Sleeve Jumper", description: "Stylish Jumper", preis: "49,99€" };
-let artikel3: ArtikelMen = { img: "shirt3.jpg", name: "Long sleeve Shirt", description: "Sytlish Jumper", preis: "40,99€" };
-let artikel4: ArtikelMen = { img: "shirt4.jpg", name: "Light Blue Shirt", description: "Stylisch blue Shirt", preis: "19,99€" };
-let artikel5: ArtikelMen = { img: "shirt5.jpg", name: "Half sleeve Jumper", description: "Stylish Jumper", preis: "39,99€" };
-let artikel6: ArtikelMen = { img: "shirt6.jpg", name: "Long sleeve Jumper", description: "Stylsih Jumper", preis: "29,99€" };
+let artikel1: ArtikelMen = { img: "shirt1.jpg", name: "Shirt Nike", description: "Sytlish Nike shirt", preis: 29.99 };
+let artikel2: ArtikelMen = { img: "shirt2.jpg", name: "Long Sleeve Jumper", description: "Stylish Jumper", preis: 49.99 };
+let artikel3: ArtikelMen = { img: "shirt3.jpg", name: "Long sleeve Shirt", description: "Sytlish Jumper", preis: 40.99 };
+let artikel4: ArtikelMen = { img: "shirt4.jpg", name: "Light Blue Shirt", description: "Stylisch blue Shirt", preis: 19.99€ };
+let artikel5: ArtikelMen = { img: "shirt5.jpg", name: "Half sleeve Jumper", description: "Stylish Jumper", preis: 39.99€ };
+let artikel6: ArtikelMen = { img: "shirt6.jpg", name: "Long sleeve Jumper", description: "Stylsih Jumper", preis: 29.99€ };
 
 
 //Artikel Men Array
@@ -23,16 +23,16 @@ interface ArtikelWomen {
     img: string;
     name: string;
     description: string;
-    preis: string;
+    preis: number;
 }
 
 //Artikel Women
-let artikel7: ArtikelWomen = { img: "shirtWomen1.jpg", name: "Pink Shirt", description: "Stylisch pink shirt", preis: "19,99€" };
-let artikel8: ArtikelWomen = { img: "shirtWomen2.jpg", name: "Purple Shirt", description: "Stylisch purple shirt", preis: "19,99€" };
-let artikel9: ArtikelWomen = { img: "shirtWomen3.jpg", name: "Light blue Shirt", description: "Stylish blue shirt", preis: "59,99€" };
-let artikel10: ArtikelWomen = { img: "shirtWomen4.jpg", name: "Simple black Shirt", description: "Simple black Shirt", preis: "7,99€" };
-let artikel11: ArtikelWomen = { img: "shirtWomen5.jpg", name: "Simple grey shirt", description: "Stylsih grey shirt", preis: "7,99€" };
-let artikel12: ArtikelWomen = { img: "shirtWomen6.jpg", name: "Red shirt", description: "Stylish Red shirt", preis: "29,99€" };
+let artikel7: ArtikelWomen = { img: "shirtWomen1.jpg", name: "Pink Shirt", description: "Stylisch pink shirt", preis: 19.99 };
+let artikel8: ArtikelWomen = { img: "shirtWomen2.jpg", name: "Purple Shirt", description: "Stylisch purple shirt", preis: 19.99 };
+let artikel9: ArtikelWomen = { img: "shirtWomen3.jpg", name: "Light blue Shirt", description: "Stylish blue shirt", preis: 59.99 };
+let artikel10: ArtikelWomen = { img: "shirtWomen4.jpg", name: "Simple black Shirt", description: "Simple black Shirt", preis: 7.99 };
+let artikel11: ArtikelWomen = { img: "shirtWomen5.jpg", name: "Simple grey shirt", description: "Stylsih grey shirt", preis: 7.99 };
+let artikel12: ArtikelWomen = { img: "shirtWomen6.jpg", name: "Red shirt", description: "Stylish Red shirt", preis: 29.99 };
 
 //Artikel Women Array
 let artikelWomen: ArtikelWomen[] = [artikel7, artikel8, artikel9, artikel10, artikel11, artikel12];
