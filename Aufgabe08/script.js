3a38d8ce7a059d3c65826f8acd0c0890257b6325
Manu <manuel.hubert@hs-furtwangen.de>
2020-06-23 21:00:17 +0200

    läuft nicht. Bis zum nächsten Semester

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -1,18 +1,22 @@
 "use strict";
 var Aufgabe08;
 (function (Aufgabe08) {
-    let submit = document.getElementById("button");
-    submit.addEventListener("click", communicate);
-    async function communicate() {
+    let senden = document.getElementById("button");
+    senden?.addEventListener("click", handleSenden);
+    async function handleSenden() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://2ndHendrix.herokuapp.com/";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
+        let url = "https://gispraktikum2020.herokuapp.com/";
         url = url + "?" + query.toString();
-        console.log(url);
-        let response = await fetch(url);
-        let requestURL = await response.text();
-        console.log(requestURL);
+        let antwort = await fetch(url);
+        let antwort2 = antwort.url;
+        console.log(antwort2);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("name: " + entry[0]);
+            console.log("value: " + entry[1]);
+        }
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,19 +1,22 @@
-namespace A08Client {
-    
-    let submitButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
-    submitButton.addEventListener("click", communicate);
+namespace Aufgabe08 {
 
-    async function communicate(): Promise<void> {
+    let senden: HTMLElement = <HTMLElement>document.getElementById("button");
+    senden?.addEventListener("click", handleSenden);
+
+    async function handleSenden(): Promise<void> {
 
         let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://2ndHendrix.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        
-        let response: Response = await fetch(url);
-        let reply: string = await response.url;
 
-        console.log(reply);
+        // tslint:disable-next-line: no-any
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        
+        let url: string = "https://gispraktikum2020.herokuapp.com/";
+
+        url = url + "?" + query.toString();
+
+        let antwort: Response = await fetch(url);
+        let antwort2: string = antwort.url;
+        console.log(antwort2);
 
         for (let entry of query) {
             console.log(entry);
@@ -21,5 +24,4 @@ namespace A08Client {
             console.log("value: " + entry[1]);
         }
     }
-    
-}
+ }
